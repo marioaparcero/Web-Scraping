82b838cddf4e20fd845d8d84fce8e48c06eb0141
Mario Aparcero <marioaparcero@hotmail.com>
2021-03-22 21:35:56 +0100

    Add: Salida de datos en varios archivos csv
    -Ahora la salida de cada grupo se guardará en un archivo csv independiente.
    -Los nombres de los archivos generados estarán saneados, con esto conseguiremos tener un control para evitar errores.
    -Todos los archivos se guardarán en una carpeta llamada csv_files/
    -Se han cambiado el nombre de las variables para que sean más legibles.
    -Se ha dejado más detallado el condicional para saber si es un alumno o un profesor.

--- webscraping.py
+++ webscraping.py
@@ -1,121 +1,98 @@
 #!/usr/bin/python3
-## crontab settings every 61 minutes
+## configuración de crontab cada 61 minutos
 ## */61 * * * * /usr/bin/python3 /home/mariotte/webscraping.py
 from selenium import webdriver
 import time
 import random
-import urllib.request
-from bs4 import BeautifulSoup
+import config
+import os
 import csv
 while True:
- # Abrir sitio web
+ ## Abrir sitio web
  driver = webdriver.Chrome()
- #url = "https://accounts.google.com/ServiceLogin?hl=es&passive=true&continue=https://groups.google.com&ec=GAZA0AM"
- #driver.get(url)
  driver.get("https://accounts.google.com/ServiceLogin?hl=es&passive=true&continue=https://groups.google.com&ec=GAZA0AM")
- # Se añade tiempo para que cargue
+ ## Se pausa la pantalla durante 2 segundos para que tengamos tiempo de confirmar que llegó a la página correcta
  time.sleep(2)
- #print(driver.result)
- # Añadir email
- Myname = driver.find_element_by_id('identifierId')
- Myname.clear()
- Myname.send_keys(config.email)
- #time.sleep(10)
- # Añadir contraseña
- # Se añade tiempo para que cargue
- time.sleep(2)
+ ## Añadir email
+ Miemail = driver.find_element_by_id('identifierId')
+ Miemail.clear()
+ Miemail.send_keys(config.email)
+ ## Presionar el botón siguiente
  button = driver.find_element_by_xpath("//div[@class='VfPpkd-RLmnJb']")
  button.click()
  time.sleep(3)
- mypass = driver.find_element_by_xpath("//input[@name='password']")
- mypass.clear()
- mypass.send_keys(config.password)
- # Presionar el boton de login
- # Se añade tiempo para que cargue
- time.sleep(2)
+ ## Añadir contraseña
+ Mipass = driver.find_element_by_xpath("//input[@name='password']")
+ Mipass.clear()
+ Mipass.send_keys(config.password)
+ ## Presionar el botón siguiente para iniciar sesión
  button = driver.find_element_by_xpath("//div[@class='VfPpkd-RLmnJb']")
  button.click()
- ### go to bonuses page
- # Se añade tiempo para que cargue
  time.sleep(9)
- ### Press bonus button
- # Todos los grupos VP4Iue wWlgrf span DPvwYc qn15kb
-#bonusbutton = driver.find_element_by_xpath("//a[@href='./all-groups']")
-#bonusbutton.click()
-#time.sleep(5)
- # Accediento a todos los grupos
+ ## Accediento a todos los grupos
  driver.get("https://groups.google.com/all-groups")
- # Cliqueando en los grupos div > wWlgrf TE2Lw skrLqf, div > u7GkXe
-#bonusbutton1 = driver.find_elements_by_xpath("//div[@class='u7GkXe']")
- #bonusbutton1.click()
- #print(bonusbutton1)
- # Obtener URL de los emails de los departamentos div > TE2Lw
- # bonusbutton1 = driver.find_elements_by_xpath("//div[@class='TE2Lw']")
- # Obtener URL de a > href de los grupos; a > eRnJIb
- nodogrupos = driver.find_elements_by_xpath("//a[@class='eRnJIb']")
- #break
- #print(bonusbutton1)
- with open('base_de_datos.csv', 'a') as csv_file:
-   writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-   #First Name,Last Name,Position,Email
-   writer.writerow(['First Name','Last Name','Position','Email', 'Departamento'])
- lista = []
- for x in nodogrupos:
-  lista = (x.get_attribute("href")+"/members")
-  # Se imprimen todos los titulos de los enlaces
-  print ("Departamento: " + x.text.strip())
-  # Se imprimen todos las url de los grupos
-  print ("Accediendo a " + lista)
-  #time.sleep(2)
-  #driver.switch_to.new_window('tab')
-  driver.execute_script("window.open()")
-  driver.switch_to.window(driver.window_handles[1])
-  driver.get(lista)
-  # Se imprimen todos los enlaces
-  #print (lista)
-  # Se pausa la pantalla durante 5 segundos para que tengamos tiempo de confirmar que llegó a la página correcta
-  #time.sleep(2)
-  # Se proporcionan los atributos de los elementos html para extraer los datos.
-  text1 = driver.find_elements_by_xpath("//a[@class='p480bb Sq3iG']")
-  text2 = driver.find_elements_by_xpath("//a[@class='p480bb Sq3iG']")
-  text3 = driver.find_elements_by_xpath("//a[@class='p480bb Sq3iG']")
-  text4 = driver.find_elements_by_xpath("//a[@class='p480bb Sq3iG']")
-  text5 = driver.find_elements_by_xpath("//a[@class='p480bb Sq3iG']")
-  text6 = driver.find_elements_by_xpath("//h1[@class='KdPHLc']")
-  #print(text2)
-  #print(text1)
-  #print(lista)
- # Se escriben los datos extraídos en un archivo csv
-  with open('base_de_datos.csv', 'a') as csv_file:
-    writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-   #First Name,Last Name,Position,Email
-    for col1,col2,col3,col4,col5 in zip(text1, text2, text3, text4, text5):
-      col2 = ""
-      if col3.text.strip().find("alu.") >= 0:
-          col3 = ("Alumno");
-      else:
-          col3 = ("Profesor");
-      col5 = text6[0].text
-      writer.writerow([col1.text.split("@")[0],col2, col3 ,col4.text.strip(), col5])
-  #driver.execute_script("window.history.go(-1)")
-  #Se cierra una ventana o pestaña
-  driver.close()
-  #Se cambia el controlador a la ventana o pestaña original
-  driver.switch_to.window(driver.window_handles[0])
-  time.sleep(1)
-  #print(lista)
-  #driver.quit()
-  #driver.back()
-  #driver.refresh()
-  #time.sleep(10)
-  #break
-  # Cliqueando en los Miembros kWxq2b VP4Iue gn3Lk class 
-  #nodogrupos = driver.find_element_by_xpath("//a[@jslog='83026; track:click; index:0;']")
-  #nodogrupos.click()
-  ###Esperando para cargar
-  #time.sleep(10)
-  #print(driver.title);
-  #print(driver.current_url);
-  ### Se cierra la página
+ ## Obtener los nodos de las páginas
+ nodospaginas = driver.find_elements_by_xpath("//div[@jsname='OCpkoe']")
+ for i in nodospaginas:
+ ## Obtener URL los grupos
+  lista = []
+  nodogrupos = driver.find_elements_by_xpath("//a[@class='eRnJIb']")
+  for x in nodogrupos:
+   lista = (x.get_attribute("href")+"/members")
+   ## Se imprimen todos los titulos de los enlaces
+   print ("Grupo: " + x.text.strip())
+   ## Declaramos el nombre del archivo de cada grupo
+   archivo = x.text.strip() + '.csv'
+   ## Declaramos el nombre del archivo saneado para evitar errores
+   dictionary = {'<':'-', '>':'-', ':':'-', '"':'-', '|':'-', '?':'-', '*':'-', '/':'-'}
+   transTable = archivo.maketrans(dictionary)
+   archivoSanitized = archivo.translate(transTable)
+   ## Se imprimen todos las url de los grupos
+   print ("Accediendo a " + lista)
+   driver.execute_script("window.open()")
+   driver.switch_to.window(driver.window_handles[1])
+   driver.get(lista)
+   ## Se proporcionan los atributos de los elementos html para extraer los datos.
+   campo1 = driver.find_elements_by_xpath("//a[@class='p480bb Sq3iG']")
+   campo2 = driver.find_elements_by_xpath("//a[@class='p480bb Sq3iG']")
+   campo3 = driver.find_elements_by_xpath("//a[@class='p480bb Sq3iG']")
+   campo4 = driver.find_elements_by_xpath("//a[@class='p480bb Sq3iG']")
+   campo5 = driver.find_elements_by_xpath("//a[@class='p480bb Sq3iG']")
+   campo6 = driver.find_elements_by_xpath("//h1[@class='KdPHLc']")
+   ## Se controla que si no hay datos se cree o no se cree el archivo
+   if len(campo1) == 0:
+     print("El archivo " + archivoSanitized + " no se ha creado porque no contiene datos. \n")
+   else:
+     ## Se crea un directorio para guardar cada archivo archivo csv en una carpeta. Se usa la librería os
+     directory = 'csv_files'
+     path = os.path.join(directory)
+     os.makedirs(path, exist_ok=True)
+     ## Se escriben los datos extraídos en los archivos csv
+     with open("csv_files/" + archivoSanitized, 'w+') as csv_file:
+       writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+       writer.writerow(['First Name','Last Name','Position','Email', 'Departamento'])
+       for col1,col2,col3,col4,col5 in zip(campo1, campo2, campo3, campo4, campo5):
+         col2 = ""
+         if "alu.iesromerovargas" in col3.text.strip():
+             col3 = ("Alumno");
+         elif "fp.iesromerovargas" in col3.text.strip():
+             col3 = ("Alumno");
+         else:
+             col3 = ("Profesor");
+         col5 = campo6[0].text
+         writer.writerow([col1.text.split("@")[0],col2, col3 ,col4.text.strip(), col5])
+     print("El archivo " + archivoSanitized + " se ha creado. \n")
+   ## Se cierra la ventana, en este caso la pestaña
+   driver.close()
+   ## Se cambia el controlador a la ventana o pestaña original
+   driver.switch_to.window(driver.window_handles[0])
+   time.sleep(1)
+  ## Presionar el botón siguiente
+  botonpaginas = driver.find_element_by_xpath("//div[@jsname='OCpkoe']")
+  botonpaginas.click()
+  time.sleep(6)
+ ## Se cierra la página
  driver.close()
- time.sleep(60*60) #Esperar 1h para cliclear de nuevo, sino estará en bucle analizando
+ print("Todos los archivos han sido creados con éxito")
+ ## Esperar 1h para analizar el bucle de nuevo
+ time.sleep(60*60)
